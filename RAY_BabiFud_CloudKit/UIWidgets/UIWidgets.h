94cd7f864f6b260bef96b155415decc4d8fdf96f
wei.mao <wei.mao@jimubox.com>
2016-10-14 17:40:27 +0800

    init project

---
+++ RAY_BabiFud_CloudKit/UIWidgets/UIWidgets.h
@@ -0,0 +1,23 @@
+//
+//  UIWidgets.h
+//  UIWidgets
+//
+//  Created by Mao on 14/10/2016.
+//  Copyright © 2016 cdts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIWidgets.
+FOUNDATION_EXPORT double UIWidgetsVersionNumber;
+
+//! Project version string for UIWidgets.
+FOUNDATION_EXPORT const unsigned char UIWidgetsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIWidgets/PublicHeader.h>
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIWidgets/PublicHeader.h>
+
+#import <UIWidgets/StarRatingControl.h>
+#import <UIWidgets/BadgeView.h>
+#import <UIWidgets/CheckedButton.h>
